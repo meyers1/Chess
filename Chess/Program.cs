8d7f1cfbe79894e8a0241490705ad96bd895c1da
meyers1 <31513085+meyers1@users.noreply.github.com>
2017-10-30 06:36:39 -0400

    small changes

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,4 +1,4 @@
-﻿  using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,6 +43,39 @@ namespace Chess
             }
         }
 
+        public static void DrawBoard()
+        {
+            Console.Clear();
+            Console.Write("       ---------------------------------");
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write("\n    " + (8 - i) + "  |");
+                for (int j = 0; j < 8; j++)
+                {
+                    if (board[i, j] == null)
+                    {
+                        Console.Write("   |");
+                    }
+                    else
+                    {
+                        Console.Write(board[i, j].Draw() + "|");
+                    }
+                }
+                if (i == 7)
+                {
+                    Console.WriteLine("\n       ---------------------------------");
+                }
+                else
+                {
+                    Console.Write("\n       |---+---+---+---+---+---+---+---|");
+                }
+            }
+            Console.WriteLine("         a   b   c   d   e   f   g   h  ");
+
+            DrawTaken(43, 0);
+            Console.SetCursorPosition(0, 20);
+        }
+
         protected static void WriteAt(string s, int x, int y)
         {
             Console.SetCursorPosition(x, y);
@@ -61,6 +94,66 @@ namespace Chess
             {
                 WriteAt(takenWhite[i].Draw(), (posX + 7), (posY + i + 2));
             }
+        }
+
+        //switches the color at the end of every turn
+        public static void EndTurn()
+        {
+            if (turn == "WHITE") turn = "BLACK";
+            else turn = "WHITE";
+        }
+
+        public static bool IsValidInput(string input)
+        {
+            if (input.Length != 4) return false;
+
+            //converts input into X and Y indices
+            int Y1 = 7 - (char.ToUpper(input[1]) - '1'); //starting row (Y)
+            int X1 = char.ToUpper(input[0]) - 'A';       //starting col (X)
+            int Y2 = 7 - (char.ToUpper(input[3]) - '1'); //ending row   (Y)
+            int X2 = char.ToUpper(input[2]) - 'A';       //ending col   (X)
+
+            //checks if input is on the board and starting and ending positions are not the same
+            if ((Y1 == Y2 && X1 == X2) ||
+                 Y1 < 0 || Y1 > 7 ||
+                 Y2 < 0 || Y2 > 7 ||
+                 X1 < 0 || X1 > 7 ||
+                 X2 < 0 || X2 > 7) return false;
+
+            //checks if the piece the player is trying to capture is their own 
+            if (board[Y2, X2] != null && board[Y2, X2].Color == turn) return false;
+
+            //checks if the piece the player is moving exists and is the correct color
+            if (board[Y1, X1] == null || board[Y1, X1].Color != turn) return false;
+
+            //checks if the piece being moved is allowed to be moved in that way
+            if (!(board[Y1, X1].IsValidMove(X1, Y1, X2, Y2, turn))) return false;
+
+            return true;
+        }
+
+        public static void Move(string input)
+        {
+            int Y1 = 7 - (char.ToUpper(input[1]) - '1'); //starting row (Y)
+            int X1 = char.ToUpper(input[0]) - 'A';       //starting col (X)
+            int Y2 = 7 - (char.ToUpper(input[3]) - '1'); //ending row   (Y)
+            int X2 = char.ToUpper(input[2]) - 'A';       //ending col   (X)
+
+            //add taken pieces to a list
+            if (board[Y2, X2] != null && board[Y2, X2].Color != turn)
+            {
+                if (board[Y2, X2].Color == "WHITE")
+                {
+                    takenWhite.Add(board[Y2, X2]);
+                }
+                else
+                {
+                    takenBlack.Add(board[Y2, X2]);
+                }
+            }
+            board[Y1, X1].HasMoved = true;
+            board[Y2, X2] = board[Y1, X1];
+            board[Y1, X1] = null;
         }
 
         public static void SetupBoard()
@@ -135,98 +228,6 @@ namespace Chess
             board[r2B.Y, r2B.X] = r2B;
         }
 
-        //switches the color at the end of every turn
-        public static void EndTurn()
-        {
-            if (turn == "WHITE") turn = "BLACK";
-            else turn = "WHITE";
-        }
-
-        public static bool IsValidInput(string input)
-        {
-            if (input.Length != 4) return false;
-
-            //converts input into X and Y indices
-            int Y1 = 7 - (char.ToUpper(input[1]) - '1'); //starting row (Y)
-            int X1 = char.ToUpper(input[0]) - 'A';       //starting col (X)
-            int Y2 = 7 - (char.ToUpper(input[3]) - '1'); //ending row   (Y)
-            int X2 = char.ToUpper(input[2]) - 'A';       //ending col   (X)
-
-            //checks if input is on the board and starting and ending positions are not the same
-            if ((Y1 == Y2 && X1 == X2) ||
-                 Y1 < 0 || Y1 > 7 ||
-                 Y2 < 0 || Y2 > 7 ||
-                 X1 < 0 || X1 > 7 ||
-                 X2 < 0 || X2 > 7) return false;
-
-            //checks if the piece the player is trying to capture is their own 
-            if (board[Y2, X2] != null && board[Y2, X2].Color == turn) return false;
-
-            //checks if the piece the player is moving exists and is the correct color
-            if (board[Y1, X1] == null || board[Y1, X1].Color != turn) return false;
-
-            //checks if the piece being moved is allowed to be moved in that way
-            if (!(board[Y1, X1].IsValidMove(X1, Y1, X2, Y2, turn))) return false;
-
-            return true;
-        }
-
-        public static void Move(string input)
-        {
-            int Y1 = 7 - (char.ToUpper(input[1]) - '1'); //starting row (Y)
-            int X1 = char.ToUpper(input[0]) - 'A';       //starting col (X)
-            int Y2 = 7 - (char.ToUpper(input[3]) - '1'); //ending row   (Y)
-            int X2 = char.ToUpper(input[2]) - 'A';       //ending col   (X)
-
-            //add taken pieces to a list
-            if (board[Y2, X2] != null && board[Y2, X2].Color != turn)
-            {
-                if (board[Y2, X2].Color == "WHITE")
-                {
-                    takenWhite.Add(board[Y2, X2]);
-                }
-                else
-                {
-                    takenBlack.Add(board[Y2, X2]);
-                }
-            }
-            board[Y1, X1].HasMoved = true;
-            board[Y2, X2] = board[Y1, X1];
-            board[Y1, X1] = null;
-        }
-
-        public static void DrawBoard()
-        {
-            Console.Clear();
-            Console.Write("       ---------------------------------");
-            for (int i = 0; i < 8; i++)
-            {
-                Console.Write("\n    " + (8 - i) + "  |");
-                for (int j = 0; j < 8; j++)
-                {
-                    if (board[i, j] == null)
-                    {
-                        Console.Write("   |");
-                    }
-                    else
-                    {
-                        Console.Write(board[i, j].Draw() + "|");
-                    }
-                }
-                if (i == 7)
-                {
-                    Console.WriteLine("\n       ---------------------------------");
-                }
-                else
-                {
-                    Console.Write("\n       |---+---+---+---+---+---+---+---|");
-                }
-            }
-            Console.WriteLine("         a   b   c   d   e   f   g   h  ");
-
-            DrawTaken(43, 0);
-            Console.SetCursorPosition(0, 20);
-        }
     }
 
     class Piece
@@ -366,7 +367,15 @@ namespace Chess
         }
         public override bool IsValidMove(int X, int Y, int newX, int newY, string Color)
         {
-            
+            if (newX == X + 1 && newY == Y + 2 ||
+                newX == X + 1 && newY == Y - 2 ||
+                newX == X + 2 && newY == Y + 1 ||
+                newX == X + 2 && newY == Y - 1 ||
+                newX == X - 1 && newY == Y + 2 ||
+                newX == X - 1 && newY == Y - 2 ||
+                newX == X - 2 && newY == Y + 1 ||
+                newX == X - 2 && newY == Y - 1) return true;
+            return false;
         }
         public override string Draw()
         {
@@ -381,6 +390,50 @@ namespace Chess
         public Bishop(int X, int Y, string Color) : base(X, Y, Color)
         {
 
+        }
+        public override bool IsValidMove(int X, int Y, int newX, int newY, string Color)
+        {
+            int disX = (newX - X), disY = (newY - Y);
+            if (Math.Abs(disX) != Math.Abs(disY)) return false;
+            if (disX > 0)
+            {
+                if (disY > 0)
+                {
+                    for (int i = 0; i < disX; i++)
+                    {
+                        if (Program.board[X + i, Y + i] != null) return false;
+                    }
+                    return true;
+                }
+                if (disY < 0)
+                {
+                    for (int i = 0; i < disX; i++)
+                    {
+                        if (Program.board[X + i, Y - i] != null) return false;
+                    }
+                    return true;
+                }
+            }
+            if (disX < 0)
+            {
+                if (disY > 0)
+                {
+                    for (int i = 0; i < disX; i++)
+                    {
+                        if (Program.board[X - i, Y + i] != null) return false;
+                    }
+                    return true;
+                }
+                if (disY < 0)
+                {
+                    for (int i = 0; i < disX; i++)
+                    {
+                        if (Program.board[X - i, Y - i] != null) return false;
+                    }
+                    return true;
+                }
+            }
+            return false;
         }
         public override string Draw()
         {
